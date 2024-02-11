cb3a63accd2ecc1c7bbcaf3ded287cc73b25e7bc
tkus1 <146398096+tkus1@users.noreply.github.com>
2024-02-11 13:10:40 -0800

    Separate setup.sh into building java project part and docker part

--- GenericNode/setup.sh
+++ GenericNode/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-mvn clean -f pom.xml
-mvn verify -f pom.xml
-cp target/GenericNode.jar docker_server/
-cp target/GenericNode.jar docker_client/
+# Build java project
+#mvn clean -f pom.xml
+#mvn verify -f pom.xml
+#cp target/GenericNode.jar docker_server/
+#cp target/GenericNode.jar docker_client/
+./buildAndCopy.sh
 docker image build -t ubuntu:TCPServer docker_server/
 docker image build -t ubuntu:TCPClient docker_client/
 echo "Stopping running TCP containers..."
