bb7ecd7b5a6b63108e889fb1894e87b8219b38e0
tkus1 <146398096+tkus1@users.noreply.github.com>
2024-02-10 20:51:01 -0800

    Add copying jar file to docker_client/

--- GenericNode/setup.sh
+++ GenericNode/setup.sh
@@ -2,6 +2,7 @@
 mvn clean -f pom.xml
 mvn verify -f pom.xml
 cp target/GenericNode.jar docker_server/
+cp target/GenericNode.jar docker_client/
 docker image build -t ubuntu:TCPServer docker_server/
 echo "Stopping all running containers..."
 docker stop $(docker ps -q)
