d2a9b777399b642491ea1fe67dfcfaf707e9267f
tkus1 <146398096+tkus1@users.noreply.github.com>
2024-02-10 20:50:30 -0800

    Implement error handling for GET for not existing key

--- GenericNode/src/main/java/genericnode/SharedResource.java
+++ GenericNode/src/main/java/genericnode/SharedResource.java
@@ -3,14 +3,20 @@ package genericnode;
 import java.util.concurrent.ConcurrentHashMap;
 
 class SharedResource {
-    private ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
+    //exception handling
+    //access null key for PUT
 
     public void put(String key, String value) {
         map.put(key, value);
     }
 
     public String get(String key) {
-        return map.get(key);
+        if(map.containsKey(key))
+        {
+            return map.get(key);
+        }
+        return "null value";
     }
     public void delete(String key) {
         map.remove(key);
@@ -20,5 +26,5 @@ class SharedResource {
     }
     public SharedResource() {
     }
-
+    //todo implement store that returns all items in the map
 }
