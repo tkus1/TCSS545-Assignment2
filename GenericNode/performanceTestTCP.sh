f7152b8b88308961ebd05e4da949d88a3101e229
tkus1 <146398096+tkus1@users.noreply.github.com>
2024-02-11 17:05:21 -0800

    Add messages to performanceTestTCP.sh and performanceTestUDP.sh

--- GenericNode/performanceTestTCP.sh
+++ GenericNode/performanceTestTCP.sh
@@ -1,2 +1,5 @@
+echo "start performance test for TCP server"
+echo "start single thread test"
 time docker exec -it TCPClient /bin/bash -c /bigtest_tc.sh
+echo "start parallel test"
 time docker exec -it TCPClient /bin/bash -c /parallelTest.sh
--- GenericNode/performanceTestUDP.sh
+++ GenericNode/performanceTestUDP.sh
@@ -1,1 +1,3 @@
+echo "start performance test for UDP server"
+echo "start single thread test"
 time docker exec -it UDPClient /bin/bash -c /bigtest_uc.sh
