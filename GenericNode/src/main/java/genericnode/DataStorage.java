75aedeaaf52a711e8a2ef6e9436fc8082f7ffd33
tkus1 <146398096+tkus1@users.noreply.github.com>
2024-02-14 17:39:44 -0800

    Add "synchronized" to all methods in DataStorage (I don't know why it works, but without it, deadlock seems to be happened)

--- GenericNode/src/main/java/genericnode/DataStorage.java
+++ GenericNode/src/main/java/genericnode/DataStorage.java
@@ -6,19 +6,19 @@ import java.util.concurrent.ConcurrentHashMap;
 public class DataStorage {
     private static final ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
 
-    public static void put(String key, String value) {
+    public static synchronized void put(String key, String value) {
         map.put(key, value);
     }
 
-    public static String get(String key) {
+    public static synchronized String get(String key) {
         return map.getOrDefault(key, "not found");
     }
 
-    public static void del(String key) {
+    public static synchronized void del(String key) {
         map.remove(key);
     }
 
-    public static ArrayList<String> store() {
+    public static synchronized ArrayList<String> store() {
         ArrayList<String> arrayList = new ArrayList<>();
         for(String key : map.keySet()) {
             String entry = "key:" + key + ":value:" + map.get(key)+ ":";
