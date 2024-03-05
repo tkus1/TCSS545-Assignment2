f592b2e96734ab438f9f4212c8145cf5ee36efb0
tkus1 <146398096+tkus1@users.noreply.github.com>
2024-03-05 14:35:58 -0800

    Modify Server (it should be reverted after separate Server into Server and DistServer)

--- GenericNode/src/main/java/genericnode/server/Server.java
+++ GenericNode/src/main/java/genericnode/server/Server.java
@@ -1,15 +1,19 @@
 package genericnode.server;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public interface Server {
 
-    void startServer(int port) throws IOException;
+    //void startServer(int port) throws IOException;
+
+    void startServer(int clientPort, int serverPort) throws IOException;
+
     void put(String key, String value) throws IOException;
     String get(String key) throws IOException;
-    void del(String key);
+    void del(String key) throws IOException;
     ArrayList<String> store() throws IOException;
     void exit() throws IOException;
-
+    void respondToServer(String key, String value, String operation, DataOutputStream outToServer) throws IOException;
 }
