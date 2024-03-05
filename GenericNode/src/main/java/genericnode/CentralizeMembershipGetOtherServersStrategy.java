bb01056da6eae024f8ee3ae0bbd41c3159c101d9
tkus1 <146398096+tkus1@users.noreply.github.com>
2024-03-05 14:28:47 -0800

    Add GetOtherServersStrategy as an interface for strategy pattern

---
+++ GenericNode/src/main/java/genericnode/CentralizeMembershipGetOtherServersStrategy.java
@@ -0,0 +1,12 @@
+package genericnode;
+
+import java.util.List;
+
+public class CentralizeMembershipGetOtherServersStrategy implements GetOtherServersStrategy{
+    @Override
+    public List<ServerConnection> getOtherServers() {
+        //todo: implement
+        return null;
+    }
+
+}
