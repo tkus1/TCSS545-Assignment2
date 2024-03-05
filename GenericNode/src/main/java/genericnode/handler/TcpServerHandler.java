5928e9b92a8b991be9acd74b538fc768399dedee
tkus1 <146398096+tkus1@users.noreply.github.com>
2024-03-05 14:34:26 -0800

    Implement TcpServerHandler

---
+++ GenericNode/src/main/java/genericnode/handler/TcpServerHandler.java
@@ -0,0 +1,52 @@
+package genericnode.handler;
+
+import genericnode.server.DistTCPServer;
+import genericnode.server.TCPServer;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class TcpServerHandler extends ServerHandler{
+    private final Socket connectionSocket;
+    public TcpServerHandler(Socket connectionSocket, TCPServer server) {
+        super(server);
+        this.connectionSocket = connectionSocket;
+    }
+
+    public void run() {
+        try {
+            System.out.println(Thread.currentThread().getId());
+            DataInputStream inFromServer = new DataInputStream(connectionSocket.getInputStream());
+            DataOutputStream outToServer = new DataOutputStream(connectionSocket.getOutputStream());
+            String operation = inFromServer.readUTF();
+            String key = null;
+            String value = null;
+            if (operation.equals("dput1")) {
+                key = inFromServer.readUTF();
+                value = inFromServer.readUTF();
+            } else if (operation.equals("dput2")) {
+                key = inFromServer.readUTF();
+                value = inFromServer.readUTF();
+            } else if (operation.equals("dputabort")) {
+                key = inFromServer.readUTF();
+                value = inFromServer.readUTF();
+            } else if (operation.equals("ddel1")) {
+                key = inFromServer.readUTF();
+            } else if (operation.equals("ddel2")) {
+                key = inFromServer.readUTF();
+            } else if (operation.equals("ddelabort")){
+                key = inFromServer.readUTF();
+            }
+            System.out.println("Receipt : Operation: " + operation + " Key: " + key + " Value: " + value);
+            System.out.println("Responding to server");
+            server.respondToServer(operation, key, value, outToServer);
+        }catch (IOException e) {
+            System.out.println("Error in TcpServerHandler");
+            System.out.println(e);
+            throw new RuntimeException(e);
+        }
+
+    }
+}
+
