24f4e873217acbe7b3bda78a1a1819e3db92d4e9
tkus1 <146398096+tkus1@users.noreply.github.com>
2024-03-10 20:44:20 -0700

    Use docker-compose to run containers in different settings.

---
+++ GenericNode/docker_client/performanceDistTestTCP.sh
@@ -0,0 +1,4 @@
+echo "Start TCP Test"
+echo "Start single thread TCP Test"
+time ./bigtest_tc.sh
+
--- GenericNode/setup_distributedTCP_containers.sh
+++ GenericNode/setup_distributedTCP_containers.sh
@@ -5,13 +5,11 @@
 # Build docker images for server and client
 docker image build -t ubuntu:distTCPServer docker_server/DistributedTCP_server/
 docker image build -t ubuntu:TCPClient docker_client/
+# Build haproxy image
 docker image build -t haproxy:distTCPHaproxy docker_server/haproxy/
 
 echo "Stopping running TCP containers..."
-docker container rm --force DistTCPServer1
-docker container rm --force DistTCPServer2
-docker container rm --force DistTCPServer3
-docker container rm --force TCPClient
+docker container ls -a | grep DistTCPServer | awk '{print $1}' | xargs docker container rm --force
 
 echo "Stopping running Haproxy container..."
 docker container rm --force Haproxy
@@ -20,12 +18,12 @@ echo "Removing DistributedTCPNetwork network..."
 docker network rm DistributedTCPNetwork
 echo "Creating DistributedTCPNetwork network..."
 docker network create DistributedTCPNetwork
-echo "Running server and client container..."
-docker container run --name DistTCPServer1 --network=DistributedTCPNetwork --rm -d -p 8081:8080 -p 8082:8081  ubuntu:distTCPServer
-docker container run --name DistTCPServer2 --network=DistributedTCPNetwork --rm -d -p 8083:8080 -p 8084:8081  ubuntu:distTCPServer
-docker container run --name DistTCPServer3 --network=DistributedTCPNetwork --rm -d -p 8085:8080 -p 8086:8081  ubuntu:distTCPServer
 
-echo "Running Haproxy container..."
-docker container run --name Haproxy --network=DistributedTCPNetwork --rm -d -p 8080:80 haproxy:distTCPHaproxy
-#docker container run --name UDPClient --network=UDPNetwork --rm -d -p 8081:8080 ubuntu:UDPClient
-docker container run --name TCPClient --network=DistributedTCPNetwork --rm -d ubuntu:TCPClient
+# Number of servers: 1
+#docker-compose -f docker_server/DistributedTCP_server/docker-compose-1server.yml up -d
+
+# Number of servers: 3
+docker-compose -f docker_server/DistributedTCP_server/docker-compose-3server.yml up -d
+
+# Number of servers: 5
+#docker-compose -f docker_server/DistributedTCP_server/docker-compose-5server.yml up -d
